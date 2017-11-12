af96b72d95c3438f48feb833f22dd51edfc6eecf
Krzysztof Matyja <matyja.krzysztof@gmail.com>
2017-11-12 18:18:20 +0000

    Add interfaces: ICloudService and ICloudTable

---
+++ EagleCalc/EagleCalc/Abstractions/ICloudService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace EagleCalc.Abstractions
+{
+    public interface ICloudService
+    {
+        Task<ICloudTable<T>> GetTableAsync<T>() where T : TableData;
+        Task SyncOfflineCacheAsync();
+    }
+}
