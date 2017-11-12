61e812a7c73f6c13175390af9ecae28d50115bcf
Krzysztof Matyja <matyja.krzysztof@gmail.com>
2017-11-12 18:27:22 +0000

    Add Helpers folder and class Locations with address to AppService

---
+++ EagleCalc/EagleCalc/Helpers/Locations.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EagleCalc.Helpers
+{
+    public static class Locations
+    {
+        public static readonly string AppServiceUrl = "https://eaglecalc.azurewebsites.net";
+    }
+}
