458037062a4c3967d676b37075a725569e3d98c4
Krzysztof Matyja <matyja.krzysztof@gmail.com>
2017-12-05 18:24:55 +0000

    Add new page ScanPage and ScanPageViewModel

---
+++ EagleCalc/EagleCalc/Pages/ScanPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using EagleCalc.ViewModels;
+using EagleCalc.Models;
+
+namespace EagleCalc.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ScanPage : ContentPage
+	{
+		public ScanPage (EagleBatch item = null)
+		{
+			InitializeComponent ();
+            BindingContext = new ScanPageViewModel(item);
+		}
+	}
+}
---
+++ EagleCalc/EagleCalc/ViewModels/ScanPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EagleCalc.Abstractions;
+using EagleCalc.Models;
+
+namespace EagleCalc.ViewModels
+{
+    public class ScanPageViewModel :BaseViewModel
+    {
+        public ScanPageViewModel(EagleBatch item = null)
+        {
+
+        }
+    }
+}
