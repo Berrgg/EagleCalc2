7df4f95e1f178af5576e46e5341434307dd316cb
Krzysztof Matyja <matyja.krzysztof@gmail.com>
2017-12-06 14:44:30 +0000

    Add SoftkeyboardDisabledEnrty to project

---
+++ EagleCalc/EagleCalc.Android/SoftkeyboardDisabledEntryRenderer.cs
@@ -0,0 +1,43 @@
+﻿using Android.Content;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using EagleCalc.Helpers;
+using EagleCalc.Droid;
+using Android.Views.InputMethods;
+
+[assembly: ExportRenderer(typeof(SoftkeyboardDisabledEntry), typeof(SoftkeyboardDisabledEntryRenderer))]
+namespace EagleCalc.Droid
+{
+    public class SoftkeyboardDisabledEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                ((SoftkeyboardDisabledEntry)e.NewElement).PropertyChanging += OnPropertyChanging;
+            }
+
+            if (e.OldElement != null)
+            {
+                ((SoftkeyboardDisabledEntry)e.OldElement).PropertyChanging -= OnPropertyChanging;
+            }
+
+            // Disable the Keyboard on Focus
+            this.Control.ShowSoftInputOnFocus = false;
+        }
+
+        private void OnPropertyChanging(object sender, PropertyChangingEventArgs propertyChangingEventArgs)
+        {
+            // Check if the view is about to get Focus
+            if (propertyChangingEventArgs.PropertyName == VisualElement.IsFocusedProperty.PropertyName)
+            {
+                // incase if the focus was moved from another Entry
+                // Forcefully dismiss the Keyboard 
+                InputMethodManager imm = (InputMethodManager)this.Context.GetSystemService(Context.InputMethodService);
+                imm.HideSoftInputFromWindow(this.Control.WindowToken, 0);
+            }
+        }
+    }
+}
---
+++ EagleCalc/EagleCalc/Helpers/SoftkeyboardDisabledEntry .cs
@@ -0,0 +1,8 @@
+﻿using Xamarin.Forms;
+
+namespace EagleCalc.Helpers
+{
+    public class SoftkeyboardDisabledEntry : Entry
+    {
+    }
+}
